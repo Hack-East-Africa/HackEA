cce9aa249341c043301fd4c80d26b71c8be68000
brunoblaise <brunoblaise94@gmail.com>
2024-09-16 23:13:32 +0200

    header

--- components/header.tsx
+++ components/header.tsx
@@ -3,12 +3,12 @@ import Image from "next/image";
 export default function Header() {
   return (
   <header >
-     <nav className="flex justify-between md">
+     <nav className="flex justify-between gap-4">
       
       <Image src='/logo.svg' alt='Logo' width={80} height={80}  priority={true}
 						object-fit="cover"
 					/>
-      <ul className="flex gap-2 md:gap-4 ">
+      <ul className="flex gap-2 md:gap-4 w-22">
         <li><a href="">About</a></li>
         <li><a href="">Schedule</a></li>
         <li><a href="">Donate</a></li>
